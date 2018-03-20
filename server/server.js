0a3a5cf52441a1c87ef17ae910a9452bbedcc0b1
Joram Kimata <joram.kimata@ega.go.tz>
2018-03-20 16:21:59 +0300

    Add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -14,6 +14,14 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+
+    });
+
+    socket.on('createMessage', (message) => {
+        console.log(message);
+    });
+
     socket.on('disconnect', () => {
         console.log('User was disconnected');
     })
